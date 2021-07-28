a41ed35c1614fa334082f4eb3fd8e7b3d447e1de
Mehdi <mehdi.medimegh@isamm.u-manouba.tn>
2021-07-28 19:15:13 +0100

    started

---
+++ component/Search.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, Image, ScrollView, TextInput, Button } from 'react-native';
+
+
+
+
+
+class Search extends React.Component{
+
+    render(){
+
+        return(
+
+            <View>
+                <TextInput placeholder="Titre du film"/>
+                <Button title="Rechercher" onPress={() => {}}/>
+            </View>
+
+            
+        )
+    }
+
+
+}
+
+export default Search
